d3e628bec062eaa7ca9d17efffdd1bc54c2633f1
Shubham Chauhan <shubhamchauhan1204.99@gmail.com>
2023-10-18 20:13:33 +0530

    PrimitiveDataType

---
+++ Introduction to Java/PrimitiveDataType.java
@@ -0,0 +1,26 @@
+public class PrimitiveDataType {
+
+    public static void main(String[] args) {
+
+        int rollNo = 12;
+        char letter = 'S';
+        float mark = 95.62f;
+        double largeDecimalNumber = 123456.9874;
+        long largeNumber = 123456789101112L;
+        boolean result = true;
+
+
+//        byte and short are also primitive data type but use are very rare
+
+
+        System.out.println("Mostly uses Primitive Data-type");
+
+        System.out.println("rollNo :" + rollNo);
+        System.out.println("letter :" + letter);
+        System.out.println("mark :" + mark);
+        System.out.println("largeDecimalNumber :" + largeDecimalNumber);
+        System.out.println("largeNumber :" + largeNumber);
+        System.out.println("result :" + result);
+    }
+
+}
