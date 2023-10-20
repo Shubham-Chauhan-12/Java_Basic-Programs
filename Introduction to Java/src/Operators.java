d64ca081f47ac6d90e1f3cab7f9b295e2824ade6
Shubham Chauhan <shubhamchauhan1204.99@gmail.com>
2023-10-21 00:09:04 +0530

    src updated

--- Introduction to Java/src/Operators.java
+++ Introduction to Java/src/Operators.java
@@ -1,3 +1,5 @@
+package src;
+
 public class Operators {
 
     public static void main(String[] args) {
--- Introduction to Java/src/PrimitiveDataType.java
+++ Introduction to Java/src/PrimitiveDataType.java
@@ -1,3 +1,5 @@
+package src;
+
 public class PrimitiveDataType {
 
     public static void main(String[] args) {
